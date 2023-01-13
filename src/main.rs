759165754c5b70d9a0ad7b63eb27708d2d7a2b6f
Kacper <kacper.lesnianski@wp.pl>
2023-01-13 17:11:43 +0100

    saving to file

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ pub mod algorithms;
 pub mod helpers;
 pub mod parsers;
 
-use crate::algorithms::{hillclimb::hillclimb, two_opt::two_opt, SnowballSampler};
+use crate::algorithms::{
+    hillclimb::hillclimb, two_opt::two_opt, EdgeMap, NodeMap, SnowballSampler,
+};
 use parsers::*;
 
 use std::{
     collections::HashMap,
+    fs::File,
+    io::Write,
     sync::{Arc, Mutex},
     thread::{self, available_parallelism},
 };
@@ -20,8 +24,22 @@ fn main() {
         SnowballSampler::new(5, 5, 3, 2, &file.distance_matrix, &two_opt, Some(2000));
     let (nodes, edges) = snowball_sampler.sample();
 
-    println!("{:?}", nodes.len());
-    println!("{:?}", edges.len());
+    let mut node_file = File::create("nodes.txt").expect("I assumed the OS will cooperate...");
+    let mut edge_file = File::create("edges.txt").expect("I assumed the OS will cooperate...");
+
+    for node in nodes {
+        let (perm, (id, h)) = node;
+        node_file
+            .write_fmt(format_args!("{};{:?};{}\n", id, perm, h))
+            .unwrap();
+    }
+
+    for edge in edges {
+        let ((a, b), w) = edge;
+        edge_file
+            .write_fmt(format_args!("{};{};{}\n", a, b, w))
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
