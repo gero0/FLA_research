e9236cd25a085077c9f32bfd9391e9ab1e2b86cc
Kacper Leśniański <kacper.lesnianski@wp.pl>
2023-02-01 10:52:22 +0100

    working binds

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from tsp_samplers import SnowballSampler
+import igraph as ig
+from igraph import Graph
 import tsplib95 as tsp
+import matplotlib.pyplot as plt
 
 problem = tsp.load('./data/bays29.tsp')
 
@@ -14,14 +17,36 @@ def create_distance_matrix(problem):
 
     return matrix
 
+def split_edge_data(edges):
+    edge_list = [ [x[0], x[1]] for x in edges]
+    weight_list = [x[2] for x in edges]
+    return edge_list, weight_list
+
 m = create_distance_matrix(problem)
 
+s = SnowballSampler(1, 20, 3, 2, m, "twoopt", 2000)
 
-s = SnowballSampler(1, 5, 3, 2, m, "twoopt", 2000)
-s.sample()
+x = []
+y = []
+
+for i in range (0, 100):
+    s.sample()
+    nodes, edges = s.get_results()
+    (edge_list, weight_list) = split_edge_data(edges)
+    g = Graph(n=len(nodes), edges=edge_list, edge_attrs={'weight': weight_list, "label" : weight_list})
+    hc = s.get_hc_calls()
+    x.append(i)
+    y.append(g.radius())
+
+plt.scatter(x, y)
+plt.savefig("fig.png")
+
+# s = SnowballSampler(100, 50, 3, 2, m, "hc", 2000)
 # s.sample()
-nodes, edges = s.get_results()
-hc_calls = s.get_hc_calls()
-print(len(nodes))
-print(len(edges))
-print(hc_calls)
+# nodes, edges = s.get_results()
+
+
+# g = Graph(n=len(nodes), edges=edge_list, edge_attrs={'weight': weight_list, "label" : weight_list})
+# ig.plot(g, target="graph.pdf")
+
+# print(g)
