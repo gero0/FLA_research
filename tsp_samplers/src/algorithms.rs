4b951e0189c9f9076e6aeeed2e7b411411498203
Kacper Leśniański <kacper.lesnianski@wp.pl>
2023-03-21 09:51:42 +0100

    small refactor of snowball

--- tsp_samplers/src/algorithms.rs
+++ tsp_samplers/src/algorithms.rs
@@ -4,10 +4,10 @@ pub type HillclimbFunction = fn(&Vec<u16>, &Vec<Vec<i32>>) -> (Vec<u16>, i32);
 pub type NodeMap = FxHashMap<Vec<u16>, (u16, i32)>;
 pub type EdgeMap = FxHashMap<(u16, u16), i32>;
 
+pub mod exhaustive_sampler;
 pub mod hillclimb;
 pub mod pwr_sampler;
 pub mod snowball_sampler;
-pub mod exhaustive_sampler;
 pub mod two_opt;
 
 pub use hillclimb::hillclimb_steepest;
